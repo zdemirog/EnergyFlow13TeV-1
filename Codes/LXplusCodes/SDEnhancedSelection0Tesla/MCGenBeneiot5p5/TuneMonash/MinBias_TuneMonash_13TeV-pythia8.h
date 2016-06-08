d19570a05a2800d4b9a44eb771df61329a43c084
caglar <caglar.zorbilmez@cern.ch>
2016-06-08 18:53:17 +0200

    Energy5p5
    Data MCDetLevel and MCGenLevel(Beneiot) 5p5 energy Cut

---
+++ Codes/LXplusCodes/SDEnhancedSelection0Tesla/MCGenBeneiot5p5/TuneMonash/MinBias_TuneMonash_13TeV-pythia8.h
@@ -0,0 +1,4 @@
+string readfilesdir= "root://eoscms.cern.ch//eos/cms/store/user/ilknur/EFlow_CFFSkim_13TeV/MC/";
+string readfilesname ="MinBias_TuneMonash_13TeV-pythia8";
+string filetype ="MC";//,"herwigpp","pythia8_Monash","pythia8_MBR","epos","qgsjetII"};
+static const Int_t readfilesnumber_in_dir =1;
